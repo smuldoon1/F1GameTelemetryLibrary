ce36c1fb2b04d6e525e4644f453e8a027e3b1585
seanmul10 <36199832+smuldoon1@users.noreply.github.com>
2022-05-08 13:23:21 +0100

    Utility methods

---
+++ Utilities/Utilities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1GameTelemetryLibrary.Util
+{
+    public class Utilities
+    {
+        public static float KphToMph(float kph)
+        {
+            return kph * 0.621371f;
+        }
+
+        public static float PsiToBar(float psi)
+        {
+            return psi * 0.0689476f;
+        }
+    }
+}
