78d6a3cce44ae0a03e2258ac953f9261814fd110
seanmul10 <36199832+smuldoon1@users.noreply.github.com>
2022-05-12 22:49:49 +0100

    Packet header is accessible

--- Packets/Public/F1Packet.cs
+++ Packets/Public/F1Packet.cs
@@ -6,8 +6,53 @@
     public abstract partial class F1Packet
     {
         /// <summary>
-        /// Struct that stores header values of every UDP packet that is sent.
+        /// Edition of the F1 game this packet is being sent by.
         /// </summary>
-        public PacketHeader Header { get { return header; } }
+        public ushort PacketFormat { get { return header.packetFormat; } }
+
+        /// <summary>
+        /// Game major version (x.00).
+        /// </summary>
+        public byte GameMajorVersion { get { return header.gameMajorVersion; } }
+
+        /// <summary>
+        /// Game minor version (1.xx).
+        /// </summary>
+        public byte GameMinorVersion { get { return header.gameMinorVersion; } }
+
+        /// <summary>
+        /// Version of this packet type.
+        /// </summary>
+        public byte PacketVersion { get { return header.packetVersion; } }
+
+        /// <summary>
+        /// Identifier for this packet type. See documentation for information about each packet type.
+        /// </summary>
+        public PacketId PacketId { get { return header.packetId; } }
+
+        /// <summary>
+        /// Unique identifier for the session. Each session generates a new identifier.
+        /// </summary>
+        public ulong SessionUID { get { return header.sessionUID; } }
+
+        /// <summary>
+        /// Session timestamp in seconds.
+        /// </summary>
+        public float SessionTime { get { return header.sessionTime; } }
+
+        /// <summary>
+        /// Identifier for the frame the packet was retrieved on.
+        /// </summary>
+        public uint FrameIdentifier { get { return header.frameIdentifier; } }
+
+        /// <summary>
+        /// Index of the players car.
+        /// </summary>
+        public byte PlayerCarIndex { get { return header.playerCarIndex; } }
+
+        /// <summary>
+        /// Index of the secondary players car (splitscreen player). 255 if there is no secondary player.
+        /// </summary>
+        public byte SecondaryPlayerCarIndex { get { return header.secondaryPlayerCarIndex; } }
     }
 }
