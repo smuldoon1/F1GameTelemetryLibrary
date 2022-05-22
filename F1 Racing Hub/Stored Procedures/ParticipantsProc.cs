0e95074f5bb1edc8e3734b92c5dad7b130fd25de
seanmul10 <36199832+smuldoon1@users.noreply.github.com>
2022-05-22 23:31:20 +0100

    Added create participant proc

---
+++ F1 Racing Hub/Stored Procedures/ParticipantsProc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1_Racing_Hub.Stored_Procedures
+{
+    internal static class ParticipantsProc
+    {
+        public static void SetParticipants(Participants[] participants)
+        {
+            foreach (Participants p in participants)
+            {
+                StoredProcedure.CallProcedure("SetParticipant",
+                    ("sessionId", p.SessionId),
+                    ("carIndex", p.CarIndex),
+                    ("aiDriverId", p.DriverId),
+                    ("name", p.Name),
+                    ("teamId", p.TeamId),
+                    ("nationalityId", p.Nationality),
+                    ("raceNumber", p.RaceNumber)
+                );
+            }
+        }
+    }
+}
