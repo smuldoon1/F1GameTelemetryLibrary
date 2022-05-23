01d82d33f3119f0b45bbbd8ed97acd1b414c14f5
seanmul10 <36199832+smuldoon1@users.noreply.github.com>
2022-05-23 22:42:51 +0100

    Update SqlValueExtensions.cs
    Added conversion methods for 64, 32, 16 and 8 bit integers.

--- F1 Racing Hub/SqlValueExtensions.cs
+++ F1 Racing Hub/SqlValueExtensions.cs
@@ -1,16 +1,88 @@
 namespace F1_Racing_Hub.Stored_Procedures
 {
+    /// <summary>
+    /// Extension methods that allow some data types to be mapped to T-SQL values. If a conversion method does not exist it is likely that the conversion is implicit.
+    /// </summary>
     public static class SqlValueExtensions
     {
-
-        public static long ToBigint(this ulong value)
+        /// <summary>
+        /// Converts an unsigned 64-bit integer to a T-SQL bigint.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static long ToSql(this ulong value)
         {
             return unchecked((long)value + long.MinValue);
         }
 
-        public static ulong FromBigint(this long value)
+        /// <summary>
+        /// Converts a T-SQL bigint to an unsigned 64-bit integer.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static ulong FromSql(this long value)
         {
             return unchecked((ulong)(value - long.MinValue));
+        }
+
+        /// <summary>
+        /// Converts an unsigned 32-bit integer to a T-SQl int.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static int ToSql(this uint value)
+        {
+            return unchecked((int)value + int.MinValue);
+        }
+
+        /// <summary>
+        /// Converts a T-SQL int to an unsigned 32-bit integer.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static uint FromSql(this int value)
+        {
+            return unchecked((uint)(value - int.MinValue));
+        }
+
+        /// <summary>
+        /// Converts an unsigned 16-bit integer to a T-SQl smallint.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static short ToSql(this ushort value)
+        {
+            return unchecked((short)(value + short.MinValue));
+        }
+
+        /// <summary>
+        /// Converts a T-SQL smallint to an unsigned 16-bit integer.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static ushort FromSql(this short value)
+        {
+            return unchecked((ushort)(value - short.MinValue));
+        }
+
+        /// <summary>
+        /// Converts a signed byte to a T-SQl tinyint.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static byte ToSql(this sbyte value)
+        {
+            return unchecked((byte)(value - sbyte.MinValue));
+        }
+
+        /// <summary>
+        /// Converts a T-SQL tinyint to a signed byte.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static sbyte FromSql(this byte value)
+        {
+            return unchecked((sbyte)(value + sbyte.MinValue));
         }
     }
 }
