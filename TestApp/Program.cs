73717b2f96ddd82b51330c050451cbdd17e7d769
seanmul10 <36199832+smuldoon1@users.noreply.github.com>
2022-05-25 20:16:05 +0100

    Delete test project

--- TestApp/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using F1GameTelemetry;
-using System.Net;
-
-// Use this app to test that the basic UdpListening is working as intended
-
-new UdpListener(IPAddress.Any, 20777);
-Console.WriteLine("Hello World!");
-Console.ReadLine();
