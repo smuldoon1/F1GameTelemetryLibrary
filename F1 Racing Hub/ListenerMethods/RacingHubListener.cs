2a6f96f365f59fb91d5282270149d8ca4625887b
seanmul10 <36199832+smuldoon1@users.noreply.github.com>
2022-05-25 22:32:25 +0100

    Added session creation proc
    Sessions are created and saved to the database

--- F1 Racing Hub/ListenerMethods/RacingHubListener.cs
+++ F1 Racing Hub/ListenerMethods/RacingHubListener.cs
@@ -19,6 +19,7 @@ namespace F1_Racing_Hub
 
             AddParticipantMethods();
             AddLapsMethods();
+            AddSessionMethods();
         }
     }
 }
---
+++ F1 Racing Hub/ListenerMethods/Session/SessionListener.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using F1GameTelemetry;
+using F1_Racing_Hub.Stored_Procedures;
+
+namespace F1_Racing_Hub
+{
+    public partial class RacingHubListener
+    {
+        public Session? currentSession;
+
+        public void AddSessionMethods()
+        {
+            listener.Subscribe(HandleSessionData);
+        }
+
+        public void HandleSessionData(SessionPacket sessionPacket)
+        {
+            currentSession = new Session()
+            {
+                SessionId = sessionPacket.SessionUID,
+                Type = (byte)sessionPacket.SessionType,
+                TrackId = sessionPacket.TrackId
+            };
+            if (!SessionProc.CheckSessionExists(currentSession))
+                SessionProc.CreateSession(currentSession);
+        }
+    }
+}
